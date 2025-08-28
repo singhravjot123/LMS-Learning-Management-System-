a6ed29b560afa5314ff74b1a65d399adaf2205ec
Ravjot Singh <singhravjot754@gmail.com>
2025-08-28 22:41:40 +0530

    start command

--- server/index.js
+++ server/index.js
@@ -29,8 +29,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+  origin: "*",  
+  credentials: true
 }));
  
 //apis
